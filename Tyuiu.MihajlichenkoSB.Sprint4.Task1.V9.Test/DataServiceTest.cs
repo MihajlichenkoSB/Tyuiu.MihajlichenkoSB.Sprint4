1da1dc3fa228eb30cc62b1b8c8f1550a7d4d2b29
Сергей Михайличенко <sm4269698@gmail.com>
2025-11-07 18:47:30 +0500

    Task 1

---
+++ Tyuiu.MihajlichenkoSB.Sprint4.Task1.V9.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint4.Task1.V9.Lib;
+namespace Tyuiu.MihajlichenkoSB.Sprint4.Task1.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] numArray = { 4, 0, 3, 12, 11 };
+
+            int res = ds.Calculate(numArray);
+            int wait = 23;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MihajlichenkoSB.Sprint4.Task1.V9/Program.cs
@@ -0,0 +1,57 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint4.Task1.V9.Lib;
+namespace Tyuiu.MihajlichenkoSB.Sprint4.Task1.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 |  Выполнил: Михайличенко С.Б. | ИИПб-25-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                *");
+            Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                             *");
+            Console.WriteLine("* Задание #1                                                               *");
+            Console.WriteLine("* Вариант #9                                                               *");
+            Console.WriteLine("* Выполнил: Михайличенко Сергей Борисович | ИИПб-25-1                      *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 13 элементов заполненный          *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 8 подсчитать произведение    *");
+            Console.WriteLine("* четных элементов массива.  С клавиатуры: 6, 3, 3, 7, 7, 7, 7, 3, 3, 6, 4, 4, 8*");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            int len;
+
+            Console.WriteLine("Введите количество элементов массива:");
+
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] namsArray = new int[len];
+
+            for (int i = 0; i < len - 1; i++) 
+            {
+                Console.WriteLine("Введите значение" + i + "элемента массива: ");
+                namsArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("массив: ");
+            for (int i = 0; i < len - 1; i++) 
+            { 
+                Console.WriteLine(namsArray[i] + "\t");
+
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            int res = ds.Calculate(namsArray);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
