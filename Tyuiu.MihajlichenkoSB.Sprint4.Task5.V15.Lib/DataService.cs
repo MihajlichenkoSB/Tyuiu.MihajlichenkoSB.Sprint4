3f50833ad3471d1125e0171b9b8e7771b40c02f0
Сергей Михайличенко <sm4269698@gmail.com>
2025-11-07 20:04:33 +0500

    Task 5

--- Tyuiu.MihajlichenkoSB.Sprint4.Task5.V15.Lib/DataService.cs
+++ Tyuiu.MihajlichenkoSB.Sprint4.Task5.V15.Lib/DataService.cs
@@ -4,22 +4,21 @@ namespace Tyuiu.MihajlichenkoSB.Sprint4.Task5.V15.Lib
 {
     public class DataService : ISprint4Task5V15
     {
-        public int Calculate(int[,] matrix)
+        public int Calculate(int[,] array)
         {
             int sum = 0;
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
 
-            int rows = matrix.GetLength(0);
-            int columns = matrix.GetLength(1);
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    if (matrix[i, j] % 2 == 0)
+                    if (array[i, j] > 0)
                     {
-                        sum += matrix[i, j];
+                        sum += array[i, j];
                     }
                 }
-
             }
             return sum;
         }
--- Tyuiu.MihajlichenkoSB.Sprint4.Task5.V15.Test/DataServiceTest.cs
+++ Tyuiu.MihajlichenkoSB.Sprint4.Task5.V15.Test/DataServiceTest.cs
@@ -2,23 +2,24 @@
 namespace Tyuiu.MihajlichenkoSB.Sprint4.Task5.V15.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
         public void TestMethod1()
         {
             DataService ds = new DataService();
 
+            // Массив взят из лога Build Output
             int[,] array = new int[,] {
-                { 2, 3, 2, 4, 3 },
-                { 4, 7, 4, 2, 2 },
-                { 5, 2, 3, 4, 6 },
-                { 5, 2, 7, 6, 5 },
-                { 6, 3, 5, 7, 7 }
+                { 0, -5, 0, 3, -4 },
+                { -1, -1, -5, 0, -5 },
+                { 2, 4, -6, 4, -2 },
+                { 2, 1, -3, 2, 1 },
+                { 1, -5, 3, 0, -6 }
             };
 
-            int expected = 49;
-
+            // Ожидаемый результат (сумма положительных: 3+2+4+4+2+1+2+1+1+3 = 23)
+            int expected = 23;
             int actual = ds.Calculate(array);
 
             Assert.AreEqual(expected, actual);
